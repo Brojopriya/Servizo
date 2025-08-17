eeb8e2780192e1cebd8017b47403f7ab293ad494
BROJOPRIYA <brojopriyanag@gmail.com>
2025-08-18 00:33:37 +0600

    almost complete

--- frontend/src/components/TechnicianDashboard.js
+++ frontend/src/components/TechnicianDashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './Dashboard.css';
+import './tech.css';
 import profile from './download.png' ;
 
 
